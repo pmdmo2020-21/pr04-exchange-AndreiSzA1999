28fd9685c42ee683eeb3cb191fe6f7a8d0b18fd7
Andrei Szambati <aszambati@gmail.com>
2020-11-10 21:21:08 +0100

    Layout Terminado

--- app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/exchange/MainActivity.kt
@@ -16,15 +16,14 @@ import es.iessaladillo.pedrojoya.exchange.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val prueba = 2
-
-
-
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
-
-
 
 }
